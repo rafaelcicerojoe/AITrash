4e0924069fb361b11bb919053cfe957f08d0803f
rafaelcicerojoe <rafaelcicerojoe@gmail.com>
2021-09-15 18:22:54 -0300

    fix - transfer learn nn

--- main.py
+++ main.py
@@ -26,40 +26,69 @@ batch_size = 16
 
 def main():
     #dataset_download()#comment if alredy downloadead
-    
+    i=2
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    change_ext(image_format,valid_folder)#comment if alredy changed the ext
-    change_ext(image_format,base_folder)
-    
-    train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    base_folder,
-    label_mode='categorical', #multiclass
-    image_size=(img_height, img_width),
-    batch_size = batch_size,
-    subset='training',
-    validation_split = 0.2,
-    seed=123
-    )
-    valid_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    base_folder,
-    label_mode='categorical', #multiclass
-    image_size=(img_height, img_width),
-    batch_size = batch_size,
-    subset='training',
-    validation_split = 0.2,
-    seed=123
-    )
-    #dt, lb = extract_features("lbp")	
-    #dhisto, lbhisto = extract_features("hist")	
-    #dhara, lbhara = extract_features("hara")
+    #change_ext(image_format,valid_folder)#comment if alredy changed the ext
+    #change_ext(image_format,base_folder)
+    '''
+    while(i<3):
+        i+=1
+        train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        base_folder,
+        label_mode='categorical', #multiclass
+        image_size=(img_height, img_width),
+        batch_size = batch_size,
+        subset='training',
+        validation_split = 0.2,
+        seed=123
+        )
+        valid_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        base_folder,
+        label_mode='categorical', #multiclass
+        image_size=(img_height, img_width),
+        batch_size = batch_size,
+        subset='validation',
+        validation_split = 0.2,
+        seed=123
+        )
+        #dt, lb = extract_features("lbp")	
+        #dhisto, lbhisto = extract_features("hist")	
+        #dhara, lbhara = extract_features("hara")
 
-    #forest_it(dhisto, lbhisto, "histograma",5)
-    #forest_it(dhara, lbhara, "haralick",3)
+        #forest_it(dhisto, lbhisto, "histograma",5)
+        #forest_it(dhara, lbhara, "haralick",3)
+        nn_no_transfer(train_dataset, valid_dataset, "no extract - augment")
+    '''
+    j=1
+    while(j<3):
+        j+=1
+        train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        base_folder,
+        label_mode='categorical', #multiclass
+        image_size=(img_height, img_width),
+        batch_size = batch_size,
+        subset='training',
+        validation_split = 0.2,
+        seed=123
+        )
+        valid_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        base_folder,
+        label_mode='categorical', #multiclass
+        image_size=(img_height, img_width),
+        batch_size = batch_size,
+        subset='validation',
+        validation_split = 0.2,
+        seed=123
+        )
+        #dt, lb = extract_features("lbp")	
+        #dhisto, lbhisto = extract_features("hist")	
+        #dhara, lbhara = extract_features("hara")
 
-
-    nn_no_transfer(train_dataset, valid_dataset, "no extract")
-    #shuffle array
-    
+        #forest_it(dhisto, lbhisto, "histograma",5)
+        #forest_it(dhara, lbhara, "haralick",3)
+        nn_transfer_learn(train_dataset, valid_dataset, "no extract - transfer learn")
+    nn_transfer_learn(train_dataset, valid_dataset, "no extract - transfer learn")
+    pass
 
     
     
--- neural_net.py
+++ neural_net.py
@@ -1,33 +1,31 @@
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, InputLayer
 from keras.models import Sequential
-from tensorflow.keras import layers
+from tensorflow.keras import layers,applications
 from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import keras
-
+import numpy as np
+import sklearn
 
 img_height = 244
 img_width = 244
 
 batch_size = 16
 
-def plot_it(H):
-    acc = H.history['accuracy']
-    val_acc = H.history['val_accuracy']
-    loss = H.history['loss']
-    val_loss = H.history['val_loss']
+def plot_it(acc,val_acc,loss,val_loss):
 
     plt.figure(figsize=(16, 8))
     plt.subplot(1, 2, 1)
-    plt.plot(range(10), acc, label='acurácia treino')
-    plt.plot(range(10), val_acc, label='acurácia validação')
+    plt.plot(range(10), len(range(10))*[acc], label='acurácia treino')
+    plt.plot(range(10), len(range(10))*[val_acc], label='acurácia validação')
     plt.legend()
     plt.title('Acurácias')
 
     plt.subplot(1, 2, 2)
-    plt.plot(range(10), loss, label='loss treino')
-    plt.plot(range(10), val_loss, label='loss validação')
+    plt.plot(range(10), len(range(10))*[loss], label='loss treino')
+    plt.plot(range(10), len(range(10))*[val_loss], label='loss validação')
     plt.legend()
     plt.title('Loss')
 
@@ -40,13 +38,14 @@ def augmentation():
     batch_size = 20
 
 
-def augment(image, label):
-    image = tf.cast(image, tf.float32)
-    image = tf.image.resize(image, [IMG_SIZE, IMG_SIZE])
-    image = (image / 255.0)
-    image = tf.image.random_crop(image, size=[IMG_SIZE, IMG_SIZE, 3])
-    image = tf.image.random_brightness(image, max_delta=0.5)
-    return image, label
+def augment():
+    data_augmentation = Sequential(
+    [
+     layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical", input_shape=(img_height, img_width, 3)),
+     layers.experimental.preprocessing.RandomRotation(0.05)
+    ]
+  )
+    return data_augmentation
 
 
 def kerasTry():
@@ -65,19 +64,18 @@ def kerasTry():
     pass
 
 def model_normie():
-    model = Sequential ([       
+    augmentation=augment()
+
+    model = Sequential ([augmentation,
         layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
         Conv2D(16,3, padding='same',activation='relu'),
-
         MaxPooling2D(),
         Conv2D(32, 3, padding='same',activation='relu'),
         MaxPooling2D(),
         Conv2D(64, 3, padding='same',activation='relu'),
         #MaxPooling2D(),
         Flatten(),
-
-        Dense(128, activation='relu',input_dim=5),
-
+        Dense(128, activation='relu',input_dim=5,kernel_regularizer='l2',bias_regularizer='l2'),
         Dense(5, activation='softmax')
         ]
     )
@@ -99,21 +97,88 @@ def nn_no_transfer(data, labels, nome ):
     model.summary()
     H = model.fit(data,batch_size=batch_size, epochs=10, validation_data=labels)
 
-    plot_it(H)
+    acc = H.history['accuracy']
+    val_acc = H.history['val_accuracy']
+    loss = H.history['loss']
+    val_loss = H.history['val_loss']
+
+    plot_it(acc,val_acc,loss,val_loss)
 
     #x_test_n = model.predict(x_test)
     y_pred = model.predict(labels)
     y_pred = np.argmax(y_pred, axis=1)
 
-    #y_true = np.concatenate([y for x, y in labels], axis=0)
-    #y_true = np.argmax(y_true, axis=1)
+    y_true = np.concatenate([y for x, y in labels], axis=0)
+    y_true = np.argmax(y_true, axis=1)
 
     print("Coletando resultados para ",nome)
-    #print(sklearn.metrics.classification_report(y_true, y_true,zero_division=1))
+    print(sklearn.metrics.classification_report(y_true, y_pred,zero_division=1))
 
     pass
 
-def nn_transfer_learn():
+def model_tl():
+    data_augmentation=augment()
+    
+    preprocess_input = keras.applications.densenet
+
+
+    baseModel = MobileNetV2(include_top=False, weights='imagenet')
+    baseModel.trainable = False
+
+
+    inputs=keras.Input(shape=(img_height, img_width, 3))
+    x = data_augmentation(inputs)
+    #x = preprocess_input(x)
+    x = baseModel(x)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dense(128, activation='relu',kernel_regularizer='l2',bias_regularizer='l2')(x)
+    x = keras.layers.Dropout(0.2)(x)
+    outputs = keras.layers.Dense(5, activation='softmax',kernel_regularizer='l2')(x)
+
+    model = keras.Model(inputs, outputs)
+
+    model.compile(optimizer='adam', loss=keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+
+    return model
+
+def nn_transfer_learn(train_dataset,validation_dataset,nome):
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    validation_dataset = validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+    model = model_tl()
+    model.summary()
+
+    H = model.fit(train_dataset,batch_size=batch_size, epochs=10, validation_data=validation_dataset)
+    '''
+    acc = H.history['accuracy']
+    val_acc = H.history['val_accuracy']
+    loss = H.history['loss']
+    val_loss = H.history['val_loss']
+
+    plot_it(acc,val_acc,loss,val_loss)
+    '''
+    for layer in model.layers[177:]:
+        layer.trainable = True
+
+    H = model.fit(train_dataset,batch_size=batch_size, epochs=20, validation_data=validation_dataset)
+    '''
+    acc += H.history['accuracy']
+    val_acc += H.history['val_accuracy']
+    loss += H.history['loss']
+    val_loss += H.history['val_loss']
+
+    plot_it(acc,val_acc,loss,val_loss)
+    '''
+    y_pred = model.predict(validation_dataset)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    y_true = np.concatenate([y for x, y in validation_dataset], axis=0)
+    y_true = np.argmax(y_true, axis=1)
+
+    print("Coletando resultados para ",nome)
+    print(sklearn.metrics.classification_report(y_true, y_pred,zero_division=1))
+
     pass
 
 def check_gpu():
